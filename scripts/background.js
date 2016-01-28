020ec2d40ef0a6ac9bd4f02560803d290d46c4f8
Rafael Biriba <biribarj@gmail.com>
2016-01-28 16:54:30 -0200

    better organization for the files

--- scripts/background.js
+++ scripts/background.js
@@ -2,7 +2,7 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
   if (msg.enmo_active === true) {
       chrome.browserAction.setIcon({
           tabId: sender.tab.id,
-          path: "icon-active.png"
+          path: "images/icon-active.png"
       });
       sendResponse({status:"test"});
   }
